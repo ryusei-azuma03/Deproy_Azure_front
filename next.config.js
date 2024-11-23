fddec0c1be929d30835d447f9f28e353f49352b7
ryusei-azuma03 <giantkilling03@gmail.com>
2024-11-23 15:05:04 +0900

    これでおわらせたい

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'standalone',
-};
-
-module.exports = nextConfig;
+    env: {
+      NEXT_PUBLIC_API_URL: 'tech0-gen-8-step3-testapp-py2-29.azurewebsites.net', // バックエンドのURLを指定
+    },
+  };
+  
+  module.exports = nextConfig;
+  
