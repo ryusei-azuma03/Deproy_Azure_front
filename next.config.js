8df666881cad942c4ad044968d07de88a192b969
ryusei-azuma03 <giantkilling03@gmail.com>
2024-11-24 07:42:17 +0900

    あさいち

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: 'standalone',
     env: {
-      NEXT_PUBLIC_API_URL: 'tech0-gen-8-step3-testapp-py2-29.azurewebsites.net', // バックエンドのURLを指定
+        NEXT_PUBLIC_API_URL: 'https://tech0-gen-8-step3-testapp-py2-29.azurewebsites.net',
     },
   };
   
